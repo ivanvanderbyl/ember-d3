172248ac493cc9fcaec7fdb0b47c3a71c62219bb
Francesco Novy <francesco@cropster.com>
2016-07-27 09:34:29 +0200

    Fix asset import in ember-cli < 2.7

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
     if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-
+      // In nested addons, app.bowerDirectory might not be available
       var bowerDirectory = app.bowerDirectory || 'bower_components';
+      // In ember-cli < 2.7, this.import is not available, so fall back to use app.import
+      var importShim = typeof this.import !== 'undefined' ? this : app;
 
-      this.import(bowerDirectory + '/d3/d3.js');
-      this.import('vendor/ember-d3/ember-d3-shim.js', {
+      importShim.import(bowerDirectory + '/d3/d3.js');
+      importShim.import('vendor/ember-d3/ember-d3-shim.js', {
         exports: {
           d3: ['default']
         }
