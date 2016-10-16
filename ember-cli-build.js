934002dfe2cc7fcaab2632b831f4fcdd2d660e89
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-10-16 14:50:22 +1100

    ESLint fix

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,9 @@
-/*jshint node:true*/
+/* jshint node:true*/
 /* global require, module */
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+let EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
+  let app = new EmberAddon(defaults, {
     // Add options here
   });
 
