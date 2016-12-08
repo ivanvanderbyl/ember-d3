5729d4a1bf8906755b5958a5ce0cab65007653bb
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-12-08 13:52:44 +1100

    Added dummy example of circles

---
+++ tests/dummy/app/routes/index.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+import Route from 'ember-route';
+import { ascending } from 'd3-array';
+
+const { run } = Ember;
+
+export default Route.extend({
+
+  setupController(controller) {
+    controller.set('circleData', this.generateRandomData());
+
+    this._updateData(controller);
+  },
+
+  _updateData(controller) {
+    run.later(this, function(controller) {
+      controller.set('circleData', this.generateRandomData());
+      this._updateData(controller);
+    }, controller, 1e3);
+  },
+
+  generateRandomData() {
+    let i = 0;
+    let total = Math.ceil(Math.random() * 10) + 2;
+    let data = [];
+
+    while (++i < total) {
+      let timestamp = Math.round((new Date()).valueOf() / 1e3) + Math.floor(Math.random() * i * 1000);
+      data.push({ value: Math.round(Math.random() * 100), timestamp });
+    }
+
+    return data.sort((a, b) => ascending(a.timestamp, b.timestamp));
+  }
+});
