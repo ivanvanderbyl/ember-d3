36b7e9aed716bdebfc002c2764a70a31014f968c
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-05-17 20:20:44 +0300

    Fixed broken tests and upgraded babel

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -3,11 +3,10 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-let App;
-
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-App = Ember.Application.extend({
+const { Application } = Ember;
+const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
