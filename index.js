1e0d1b133d172cf1f87076c9ff1f0b7ecfe6286f
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-05-17 17:24:11 +0300

    Refactored to use rollup for compilation of everything

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 /* eslint "no-var":off, "ember-suave/prefer-destructuring":off */
-var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
-var path = require('path');
-var rename = require('broccoli-stew').rename;
-var RecastFilter = require('./lib/recast-filter');
-var rewriteAMDFunction = require('./lib/rewrite-amd-definition');
-var replaceVersionString = require('./lib/replace-version-string');
-var d3DepsForPackage = require('./lib/d3-deps-for-package');
-var d3Version = require('./lib/d3-version');
-var inclusionFilter = require('./lib/inclusion-filter');
-var exclusionFilter = require('./lib/exclusion-filter');
 
+var mergeTrees = require('broccoli-merge-trees');
+var d3DepsForPackage = require('./lib/d3-deps-for-package');
+var Rollup = require('broccoli-rollup');
+var path = require('path');
+var resolve = require('rollup-plugin-node-resolve');
+var rollupExternalPackage = require('./lib/rollup-external-package');
 module.exports = {
   isDevelopingAddon() {
     return false;
@@ -18,105 +13,46 @@ module.exports = {
 
   name: 'ember-d3',
 
-  /**
-   * Array of d3 packages to load
-   *
-   * @private
-   * @type {Array<String>}
-   */
-  d3Modules: [],
-
-  /**
-   * `import()` taken from ember-cli 2.7
-   * @private
-   */
-  import(asset, options) {
-    var app = this.app;
-    while (app.app) {
-      app = app.app;
-    }
-
-    app.import(asset, options);
-  },
-
-  included(app) {
-    this._super.included && this._super.included.apply(this, arguments);
-    this.app = app;
-
-    /*
-      Find all dependencies of `d3`
-     */
-    var config = app.project.config(app.env) || {};
-    var addonConfig = config[this.name] || {};
-    this.d3Modules = this.getD3Modules(addonConfig);
-
-    /*
-      This essentially means we'll skip importing this package twice, if it's
-      a dependency of another package.
-     */
-    if (!this.import) {
-      if (this.isDevelopingAddon()) {
-        this.ui.writeWarnLine('[ember-d3] skipping included hook for', this.name);
-      }
-
-      return;
-    }
-
-    /*
-      Actually import the vendor tree packages to our app.
-     */
-    var _this = this;
-    this.d3Modules.forEach(function(pkg) {
-      _this.import(path.posix.join('vendor', pkg.name, `${ pkg.name }.js`));
-    });
-
-    app.import(path.posix.join('vendor', 'ember-d3', 'register-d3-version.js'));
-  },
-
-  getD3Modules(config) {
-    var allModules = this._getAllD3Modules();
-    var onlyModules = config.only || [];
-    var exceptModules = config.except || [];
-
-    return allModules
-      .filter(inclusionFilter(onlyModules))
-      .filter(exclusionFilter(exceptModules));
-  },
-
   _getAllD3Modules() {
     return d3DepsForPackage('d3', this.parent.nodeModulesPath, this.ui);
   },
 
-  treeForVendor(tree) {
-    var trees = [];
+  included() {
+    let d3Modules = this._getAllD3Modules();
 
-    if (tree) {
-      var versionTree = new RecastFilter(tree, null, replaceVersionString, {
-        version: d3Version('d3', this.parent.nodeModulesPath)
-      });
-
-      trees.push(rename(versionTree, function() {
-        return path.posix.join('ember-d3', 'register-d3-version.js');
-      }));
-    }
-
-    this.d3Modules.forEach(function(pkg) {
-      if (!(pkg && pkg.path)) {
-        return;
-      }
-
-      var tree = new Funnel(pkg.path, {
-        include: [pkg.buildPath],
-        destDir: `/${  pkg.name}`,
-        annotation: `Funnel: D3 Source [${  pkg.name  }]`
-      });
-
-      var srcTree = new RecastFilter(tree, pkg.name, rewriteAMDFunction);
-      trees.push(rename(srcTree, function() {
-        return `/${  pkg.name  }/${  pkg.name  }.js`;
-      }));
+    // Import each D3 module
+    d3Modules.forEach((module) => {
+      this.import(path.posix.join('vendor', `${module.name }.js`));
     });
 
+    // Import D3 include for bundled imports
+    this.import(path.posix.join('vendor', 'd3.js'));
+  },
+
+  treeForVendor() {
+    let d3Modules = this._getAllD3Modules();
+    let dependencies = d3Modules.map((dep) => dep.name);
+
+    // Rollup each D3 library
+    let entry;
+
+    let tree = 'app';
+    let trees = d3Modules.map((module) => {
+      entry = path.join('node_modules', module.name, 'index.js');
+      let t = rollupExternalPackage(tree, module.name, entry, dependencies);
+
+      tree = t;
+
+      return t;
+    });
+
+    let d3SourcePath = 'node_modules/d3';
+    entry = path.join(d3SourcePath, 'index.js');
+
+    let d3Tree = rollupExternalPackage(tree, 'd3', entry, dependencies);
+
+    trees.push(d3Tree);
     return mergeTrees(trees);
   }
+
 };
