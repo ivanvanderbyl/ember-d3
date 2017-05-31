6e541c0707a1bf439c5bcc7065a411c51fbf7827
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-05-31 17:22:32 +0300

    Updated dependencies

--- config/changelog.js
+++ config/changelog.js
@@ -6,10 +6,12 @@ module.exports = {
   // angular style guide: https://github.com/angular/angular.js/blob/v1.4.8/CONTRIBUTING.md#commit
   // jquery style guide: https://contribute.jquery.org/commits-and-pull-requests/#commit-guidelines
   // ember style guide: https://github.com/emberjs/ember.js/blob/master/CONTRIBUTING.md#commit-tagging
-  style: 'angular', // 'ember' 'jquery'
+  style: 'ember', // 'ember' 'jquery'
 
   head: 'master',
-  base: '-last', // a branch or tag name, `-last` defaults to the version in package.json
+  base: 'v0.1.0', // a branch or tag name, `-last` defaults to the version in package.json
+
+  // project: 'ivanvanderbyl/ember-d3',
 
   hooks: {
     /*
