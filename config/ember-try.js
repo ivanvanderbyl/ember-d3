1d656ef89e6da395ff78af2e85ab1510c0d09e8c
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-02-01 11:31:35 -0600

    Test multiple d3 releases

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,67 +1,60 @@
 module.exports = {
-	useYarn: true,
-	scenarios: [
-		{
-			name: 'ember-lts-2.16',
-			npm: {
-				devDependencies: {
-					'ember-source': '~2.16.0'
-				}
-			}
-		},
-		{
-			name: 'ember-release',
-			bower: {
-				dependencies: {
-					ember: 'components/ember#release'
-				},
-				resolutions: {
-					ember: 'release'
-				}
-			},
-			npm: {
-				devDependencies: {
-					'ember-source': null
-				}
-			}
-		},
-		{
-			name: 'ember-beta',
-			bower: {
-				dependencies: {
-					ember: 'components/ember#beta'
-				},
-				resolutions: {
-					ember: 'beta'
-				}
-			},
-			npm: {
-				devDependencies: {
-					'ember-source': null
-				}
-			}
-		},
-		{
-			name: 'ember-canary',
-			bower: {
-				dependencies: {
-					ember: 'components/ember#canary'
-				},
-				resolutions: {
-					ember: 'canary'
-				}
-			},
-			npm: {
-				devDependencies: {
-					'ember-source': null
-				}
-			}
-		},
-		{
-			name: 'ember-default',
-			npm: {
-				devDependencies: {}
-			}
-		}
-	]
+  useYarn: true,
+  scenarios: [
+    {
+      name: 'ember-lts-2.16',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.16.0',
+          d3: '4.0.0'
+        }
+      }
+    },
+
+    {
+      name: 'ember-2.18-d3-4.1',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.18.0',
+          d3: '4.1.0'
+        }
+      }
+    },
+
+    {
+      name: 'ember-2.18-d3-4.2',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.18.0',
+          d3: '4.2.0'
+        }
+      }
+    },
+
+    {
+      name: 'ember-2.18-d3-4.7',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.18.0',
+          d3: '4.7.0'
+        }
+      }
+    },
+
+    {
+      name: 'ember-2.18-d3-4.4.13',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.18.0',
+          d3: '4.13.0'
+        }
+      }
+    },
+    {
+      name: 'ember-default',
+      npm: {
+        devDependencies: {}
+      }
+    }
+  ]
 }
