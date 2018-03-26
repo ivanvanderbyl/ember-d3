b1c17aa1154ea52594df26ef45c8f5dd437fb1be
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-03-26 12:52:11 +1100

    Fix try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -22,16 +22,6 @@ module.exports = {
     },
 
     {
-      name: 'ember-2.18-d3-4.2',
-      npm: {
-        devDependencies: {
-          'ember-source': '~2.18.0',
-          d3: '4.2.0'
-        }
-      }
-    },
-
-    {
       name: 'ember-2.18-d3-4.7',
       npm: {
         devDependencies: {
