e5feeffc3ef1e615647cda40a48b4e793260fdbe
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2019-01-18 15:25:00 -0600

    Apply update patch

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,80 +1,32 @@
-const getChannelURL = require('ember-source-channel-url')
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
 
 module.exports = function() {
   return Promise.all([
     getChannelURL('release'),
     getChannelURL('beta'),
     getChannelURL('canary')
-  ]).then(urls => {
+  ]).then((urls) => {
     return {
-      useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.16',
+          name: 'ember-lts-2.18',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
           },
           npm: {
             devDependencies: {
               '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.16.0',
-              d3: '4.0.0'
+              'ember-source': '~2.18.0'
             }
           }
         },
-
         {
-          name: 'ember-2.18-d3-4.1',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
+          name: 'ember-lts-3.4',
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0',
-              d3: '4.1.0'
-            }
-          }
-        },
-
-        {
-          name: 'ember-2.18-d3-4.7',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0',
-              d3: '4.7.0'
-            }
-          }
-        },
-
-        {
-          name: 'ember-2.18-d3-4.4.13',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0',
-              d3: '4.13.0'
-            }
-          }
-        },
-
-        {
-          name: 'ember-3.0-d3-5.0.0',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.0.0',
-              d3: '^5.0.0'
-            }
-          }
-        },
-
-        {
-          name: 'ember-3.0-d3-4.1',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.0.0'
-            },
-            dependencies: {
-              d3: '^4.1.0'
+              'ember-source': '~3.4.0'
             }
           }
         },
@@ -102,6 +54,10 @@ module.exports = function() {
             }
           }
         },
+        // The default `.travis.yml` runs this scenario via `npm test`,
+        // not via `ember try`. It's still included here so that running
+        // `ember try:each` manually or from a customized CI config will run it
+        // along with all the other scenarios.
         {
           name: 'ember-default',
           npm: {
@@ -122,6 +78,6 @@ module.exports = function() {
           }
         }
       ]
-    }
-  })
-}
+    };
+  });
+};
